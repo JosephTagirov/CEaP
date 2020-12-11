7118b19145921b6626754d1002095db679b0f567
JosephTagirov <71585156+JosephTagirov@users.noreply.github.com>
2020-12-11 15:50:51 +0300

    Create 3.py

---
+++ LW_3/3.py
@@ -0,0 +1,5 @@
+friends = ['Valery', 'Thomas', 'Richard', 'Winston', 'Joseph', 'Sam', 'Charles']
+popular_names = ['James', 'John', 'Robert', 'Michael', 'William', 'David', 'Richard', 'Joseph', 'Thomas', 'Charles']
+for i in range (len(friends)):
+    if friends[i] in popular_names:
+        print(friends[i])
